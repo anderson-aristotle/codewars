41b586ed3b1ab37967efdbd8918c225272960c9a
rex <anderson.aristotle@gmail.com>
2019-05-18 15:32:56 -0400

    tested iteration method in node

--- code.js
+++ code.js
@@ -49,3 +49,8 @@ function sumTwoSmallestNumbers(numbers) {
 // Given an array of integers, remove the smallest value.Do not mutate the original array / list.If there are multiple elements with the same value, 
 // remove the one with a lower index.If you get an empty array / list, return an empty array / list.
 // Don't change the order of the elements that are left.
+
+let ratings = [5, 3, 2, 1, 4]
+for (i = 0; i < array.ratings; i++) {
+    console.log(array[index])
+} 
